f894bf8b051cc2eff015f1860b686f763c145c60
delaram <delaram.yousefii4149@gmail.com>
2025-03-10 14:43:30 +0330

    add

--- src/route/HomePage.jsx
+++ src/route/HomePage.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-import ProductPage from '../component/ProductPage'
-import { Link } from 'react-router-dom'
- 
- 
- const HomePage = () => {
-    return (
-     <div>
-      <Link to={'/test'}> go to the test page</Link>
-       <ProductPage/>
-     </div>
-   )
- }
- 
- export default HomePage
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
