be9fd7d754f3e32dfbad402457e104522872a2a8
delaram <delaram.yousefii4149@gmail.com>
2025-03-10 14:50:53 +0330

    add

--- src/route/HomePage.jsx
+++ src/route/HomePage.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const HomePage = () => {
+  const [number , setNumber]=useState(0);
   return (
-    <div>HomePage</div>
+    <div>
+      <button onClick={()=>setNumber(number + 1)} className='bg-gray-200 m-2 p-2 rounded-md'> increase </button>
+
+      <p>{number}</p>
+    </div>
+
   )
 }
 
