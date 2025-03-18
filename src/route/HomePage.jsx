064c58498cc13193286c13bdc6be6837c572a170
delaram <delaram.yousefii4149@gmail.com>
2025-03-18 16:39:09 +0330

    11

--- src/route/HomePage.jsx
+++ src/route/HomePage.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const gi = () => {
-  return (
-    <div>gi</div>
-  )
-}
-
-export default gi
+ import React from 'react'
+ 
+ const HomePage = () => {
+   return (
+     <div>HomePage</div>
+   )
+ }
+ 
+ export default HomePage
