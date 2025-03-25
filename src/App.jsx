f3e59ad24880edec34e25ce76a7349cc0e245a82
delaram <delaram.yousefii4149@gmail.com>
2025-03-25 19:11:14 +0330

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
- import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './route/HomePage'
-import NotFound from './route/NotFound'
-import BasketPage from './route/BasketPage'
-import Header from './component/Header'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Test from './route/Test'
-import AddProductPage from './route/AddProductPage'
- 
- const App = () => {
-   const queryClient= new QueryClient({
-    defaultOptions : {
-      queries : {
-        staleTime : 10000,
-        retry : false,
-        gcTime : 5000,
-      }
-    }
-   })
-   return (
-    <>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools/>
-    <Header/>
-  <Routes>
-<Route path='/' element={<HomePage/>}/>
-<Route path='/test' element={<Test/>}/>
-<Route path='/add-product' element={<AddProductPage/>}/>
-<Route path='*' element={<NotFound/>}/>
-<Route path='/basket' element={<BasketPage/>}/>
+import React from 'react'
 
-  </Routes>
-  </QueryClientProvider>
-    </>
-   )
- }
- 
- export default App
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/component/HeaderCopy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderCopy = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default HeaderCopy
